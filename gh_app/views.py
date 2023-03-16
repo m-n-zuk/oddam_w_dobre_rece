038bf11a7065eda50a1a5ba83d38d79782dd9593
m-n-zuk <magdalena.n.zuk@gmail.com>
2023-03-16 18:27:31 +0100

    create simple version of views: landing page, login, register, add donation

--- gh_app/views.py
+++ gh_app/views.py
@@ -3,6 +3,21 @@ from django.shortcuts import render
 from django.views import View
 
 
-class MainView(View):
+class LandingPage(View):
     def get(self, request):
         return render(request, 'index.html')
+
+
+class AddDonation(View):
+    def get(self, request):
+        return render(request, 'form.html')
+
+
+class Login(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+
+class Register(View):
+    def get(self, request):
+        return render(request, 'register.html')
