f981da70f818abf9dff019e7c02c04f015c8c130
m-n-zuk <magdalena.n.zuk@gmail.com>
2023-03-17 21:48:55 +0100

    quantity of donations & institutions on landing page

--- gh_app/views.py
+++ gh_app/views.py
@@ -2,10 +2,21 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
+from gh_app.models import Donation, Institution
+
 
 class LandingPage(View):
     def get(self, request):
-        return render(request, 'index.html')
+
+        donations = Donation.objects.all()
+        donations_quantity = 0
+
+        for donation in donations:
+            donations_quantity += donation.quantity
+
+        institutions_quantity = Institution.objects.all().count() - Institution.objects.filter(donation=None).count()
+
+        return render(request, 'index.html', {"d_quantity": donations_quantity, "i_quantity": institutions_quantity})
 
 
 class AddDonation(View):
